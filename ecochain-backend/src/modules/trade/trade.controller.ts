78f33a9304e4c56f1663c65faebd10fb0c1756b0
ZaidWiztech <muhammad.zaid@wiztech.pk>
2024-06-09 13:06:53 +0500

    => trade apis

--- ecochain-backend/src/modules/trade/trade.controller.ts
+++ ecochain-backend/src/modules/trade/trade.controller.ts
@@ -2,7 +2,7 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Trade } from 'src/models/Trade';
 import { TradeService } from './trade.service';
@@ -15,29 +15,44 @@ export class TradeController {
     constructor(private readonly tradeService: TradeService) {
     }
 
-    @Post('/initiateTrade') 
+    @Post('/initiateTrade')
     @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
     @ApiResponse({ status: 403, description: 'Forbidden.' })
     @ApiResponse({ status: 400, description: 'Bad Request.' })
-    async initiateTrade( @Body() body: Trade) {
+    async initiateTrade(@Body() body: Trade) {
         return await this.tradeService.initiateTrade(body);
     }
 
     @Get('/getMyTrades')
     async getMyTrades(@Query() query: any) {
-        const {sellerId} = query;
+        const { sellerId } = query;
         return this.tradeService.getMyTrades(sellerId);
     }
 
     @Get('/getAllTrades')
     async getAllTrades(@Query() query: any) {
-        const {sellerId} = query;
+        const { sellerId } = query;
         return this.tradeService.getAllTrades(sellerId);
     }
 
     @Get('/getAllActiveTrades')
     async getAllActiveTrades(@Query() query: any) {
-        const {sellerId} = query;
+        const { sellerId } = query;
         return this.tradeService.getAllActiveTrades(sellerId);
     }
- }
+    @Delete('/deleteTrade')
+    async deleteTrade(@Query() query: any) {
+        const { tradeId } = query;
+        return this.tradeService.deleteTrade(tradeId);
+    }
+    @Post('/updateTrade')
+    async updateTrade(@Query() query: any, @Body() trade: Trade) {
+        const { tradeId } = query;
+        return this.tradeService.update(tradeId, trade);
+    }  
+    @Get('/buy')
+    async buy(@Query() query: any) {
+        const { tradeId, units } = query;
+        return this.tradeService.buy(tradeId, units);
+    }
+}
--- ecochain-backend/src/modules/trade/trade.service.ts
+++ ecochain-backend/src/modules/trade/trade.service.ts
@@ -7,6 +7,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { Op } from 'sequelize';
 import { TRADE } from 'src/constants/constants';
 import { Trade } from 'src/models/Trade';
+import { User } from 'src/models/User';
 
 @Injectable()
 export class TradeService {
@@ -18,19 +19,50 @@ export class TradeService {
     }
 
     async getMyTrades(_sellerId: number) {
-        return this.tradeRepo.findAll({where: {sellerId: _sellerId}})
+        return this.tradeRepo.findAll({ where: { sellerId: _sellerId, deleted: false }, include: [User] },)
     }
 
     async getAllTrades(_sellerId: number) {
-        return this.tradeRepo.findAll({where: {sellerId: {
-            [Op.ne]: _sellerId,
-        }}});
+        return this.tradeRepo.findAll({
+            where: {
+                sellerId: {
+                    [Op.ne]: _sellerId,
+                }, deleted: false
+            }, include: [User]
+        });
     }
 
     async getAllActiveTrades(_sellerId: number) {
-        return this.tradeRepo.findAll({where: {sellerId: {
-            [Op.ne]: _sellerId,
-        }, active: true }});
+        return this.tradeRepo.findAll({
+            where: {
+                sellerId: {
+                    [Op.ne]: _sellerId,
+                }, active: true, deleted: false
+            }, include: [User]
+        });
     }
 
- }
+    async getTradeByTradeId(tradeId: number) {
+        return this.tradeRepo.findOne({where: {tradeId: tradeId}, include: [User]})
+    }
+
+    async deleteTrade(tradeId: number) {
+        let trade = await this.getTradeByTradeId(tradeId);
+        trade.deleted = true;
+        trade.active = false;
+        trade.save();
+    }
+
+    async update(id: number, entity: Trade) {
+        return this.tradeRepo.update({ ...entity }, { where: { tradeId: id } });
+    }
+
+    async buy(tradeId: number, units: number) {
+        let trade = await this.getTradeByTradeId(tradeId);
+        trade.totalSold = units;
+        trade.unitsForSale -= units;
+        if (trade.unitsForSale && trade.unitsForSale <= 0)
+            trade.active = false;
+    }
+
+}
